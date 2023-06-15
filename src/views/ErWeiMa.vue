b4a407d389cde05918472852a17a94993f4d328a
wanghaoyu <3275568354@qq.com>
2023-06-15 13:26:26 +0800

    增加用户体验，添加走马灯

--- src/views/ErWeiMa.vue
+++ src/views/ErWeiMa.vue
@@ -1,30 +1,29 @@
 <template>
-  <div className="login" id="login-app">
-    <div className="login-box">
-      <img src="@/images/login/login-l.png" alt="" className="xiashi"
-           style="border-right: 2px solid  rgb(235, 239, 240);border-radius: 12px;">
+  <div class="login" id="login-app">
+    <div class="login-box">
 
-      <!--        <div class="block text-center zoumadeng">-->
-      <!--          <el-carousel height="474.38px" style="border-radius: 12px">-->
-      <!--            <el-carousel-item v-for="item in arrImg" :key="item">-->
-      <!--              &lt;!&ndash;            <h3 class="small justify-center" text="2xl">{{ item }}</h3>&ndash;&gt;-->
-      <!--              &lt;!&ndash;            <img src="@/images/Tips{{item}}.jpg" alt="" style="height: 474.38px">&ndash;&gt;-->
-      <!--              <el-image :src="item" style="height: 474.38px">-->
 
-      <!--              </el-image>-->
-      <!--            </el-carousel-item>-->
-      <!--          </el-carousel>-->
-      <!--        </div>-->
+      <div class="block text-center zoumadeng" style="margin-right: 3px">
+        <el-carousel height="474.38px" style="border-radius: 12px">
+          <el-carousel-item v-for="item in arrImg" :key="item">
+            <!--            <h3 class="small justify-center" text="2xl">{{ item }}</h3>-->
+            <!--            <img src="@/images/Tips{{item}}.jpg" alt="" style="height: 474.38px">-->
+            <el-image :src="item" style="height: 474.38px">
 
-      <div className="login-form" style="position: relative;">
-        <div className="fanhuidenglu">
+            </el-image>
+          </el-carousel-item>
+        </el-carousel>
+      </div>
+
+      <div class="login-form" style="position: relative;">
+        <div class="fanhuidenglu">
           <img src="../images/tips2.png" alt="" style="width:200px; position: absolute; top: 15px; right: 55px;">
           <img src="../images/erweima.png" alt=""
                style="width: 50px; height: 50px;  position: absolute; top: 2px; right: 2px;"
                @click="fanhui"
           >
         </div>
-        <img src="../images/image-removebg-preview.png" alt="">
+        <img src="../images/image-removebg-preview.png" alt="" >
       </div>
     </div>
 
@@ -36,19 +35,19 @@
 
 export default {
 
-  // data() {
-  //   return {
-  //     arrImg:[
-  //       //动态添加数据是require
-  //       require("../images/Tips1.jpg"),
-  //       require("../images/Tips2.jpg"),
-  //       require("../images/Tips3.jpg"),
-  //       require("../images/Tips4.jpg")
-  //     ]
-  //   }
-  // },
+  data() {
+    return {
+      arrImg:[
+        //动态添加数据是require
+        require("../images/Tips1.jpg"),
+        require("../images/Tips2.jpg"),
+        require("../images/Tips3.jpg"),
+        require("../images/Tips4.jpg")
+      ]
+    }
+  },
   methods: {
-    fanhui() {
+    fanhui(){
       this.$router.push('/')
     },
   }
