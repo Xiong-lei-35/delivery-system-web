56b69843bfc494652b3b68e5faae955f0702dcf0
wanghaoyu <3275568354@qq.com>
2023-06-15 13:22:42 +0800

    增加用户体验，添加开机动画

--- src/views/ErWeiMa.vue
+++ src/views/ErWeiMa.vue
@@ -1,7 +1,8 @@
 <template>
   <div className="login" id="login-app">
     <div className="login-box">
-      <img src="@/images/login/login-l.png" alt="" className="xiashi">
+      <img src="@/images/login/login-l.png" alt="" className="xiashi"
+           style="border-right: 2px solid  rgb(235, 239, 240);border-radius: 12px;">
 
       <!--        <div class="block text-center zoumadeng">-->
       <!--          <el-carousel height="474.38px" style="border-radius: 12px">-->
